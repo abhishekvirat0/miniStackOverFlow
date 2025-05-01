ea9b0a532be094c9cabb02619d64180cef0e355f
Abhishek <abhishekvirat0@gmail.com>
2025-05-01 11:30:19 +0200

    Postgres added for render

--- miniStackOverFlow/settings.py
+++ miniStackOverFlow/settings.py
@@ -125,12 +125,25 @@ WSGI_APPLICATION = 'miniStackOverFlow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# In case of Production
+if os.getenv("RENDER") == "true":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'ministackoverflow',
+            'USER': 'ministackoverflow_user',
+            'PASSWORD': 'XDK5LKoqlvcy6LnHRC6QCCWM7iIWi6h1',
+            'HOST': 'dpg-d0911tc9c44c73ab7oeg-a',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
